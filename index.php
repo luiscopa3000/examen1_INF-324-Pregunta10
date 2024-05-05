8d83855a4769aa174ff209da08818b71fe1c23ef
luiscopa3000 <plan300014@gmail.com>
2024-05-05 11:36:07 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Problema 10</title>
+</head>
+
+<body>
+    <div class="container">
+        <?php echo "<p>Hola mundo!</p>" ?>
+    </div>
+    <style>
+        p {}
+    </style>
+</body>
+
+</html>
